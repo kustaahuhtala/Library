533edb4e90f2ea8c4e7e081ec879316395cec440
Kustaa Huhtala <kustaa.huhtala@houston-inc.com>
2011-09-21 13:52:15 +0300

    Refactored CRUD functionalities

--- src/main/java/com/houston/library/pages/CreateItem.java
+++ src/main/java/com/houston/library/pages/CreateItem.java
@@ -1,7 +1,7 @@
 package com.houston.library.pages;
 
 import org.apache.tapestry5.annotations.InjectPage;
-import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,22 +15,39 @@ import com.houston.library.repository.ItemRepository;
  */
 public class CreateItem {
 
-	@Property
+	private static final Logger logger = LoggerFactory.getLogger(CreateItem.class);
+
+	@Persist
 	private Item item;
 
 	@Inject
 	@Autowired
 	private ItemRepository itemRepository;
 
-	private Logger logger = LoggerFactory.getLogger(CreateItem.class);
-
 	@InjectPage
 	private Index indexPage;
 
+	public void onActivate(Long id) {
+		if (id.equals(0L)) {
+			item = new Item();
+		} else {
+			logger.debug("Finding item with id {}", id);
+			item = itemRepository.findOne(id);
+		}
+	}
+
 	public Index onSuccessFromCreateItem() {
-		itemRepository.save(item);
 		logger.debug("Success from create item form.");
+		itemRepository.save(item);
 		return indexPage;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
 	}
 
 }
--- src/main/java/com/houston/library/pages/Index.java
+++ src/main/java/com/houston/library/pages/Index.java
@@ -1,5 +1,7 @@
 package com.houston.library.pages;
 
+import org.apache.tapestry5.annotations.InjectPage;
+import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +21,22 @@ public class Index {
 	@Inject
 	private ItemRepository itemRepository;
 
+	@InjectPage
+	private CreateItem createItem;
+
 	public Iterable<Item> getItems() {
 		return itemRepository.findAll();
+	}
+
+	@OnEvent("edit")
+	public CreateItem edit(Long id) {
+		createItem.setItem(itemRepository.findOne(id));
+		return createItem;
+	}
+
+	@OnEvent("remove")
+	public void remove(Long id) {
+		itemRepository.delete(itemRepository.findOne(id));
 	}
 
 }
